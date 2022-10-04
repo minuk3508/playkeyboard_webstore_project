e588fa0ea77339fe9cd345a2e9d16aa48ea2f7a5
손소희 <ssh30510044@gmail.com>
2022-10-04 21:47:32 +0900

    Add: products 페이지 ui 구현중

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Container>
+      <LogoBox>
+        <img
+          src="https://plkey.app/assets/images/logo.png"
+          alt="playkey board logo"
+        />
+      </LogoBox>
+      <SearchBox>
+        <img
+          src="https://img.icons8.com/ios-glyphs/344/search--v1.png"
+          alt="search icon"
+        />
+      </SearchBox>
+    </Container>
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  position: sticky;
+  top: 0;
+  width: 100%;
+`;
+
+const LogoBox = styled.div`
+  img {
+    width: 140px;
+  }
+`;
+
+const SearchBox = styled.div`
+  img {
+    width: 20px;
+  }
+`;
 
 export default Header;
