f97b761c39bc69711783a97644a3a044aea34d4c
hoonjo1 <hoonjo1@gmail.com>
2022-10-06 15:42:25 +0900

    color 변경 fadeout 적용

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import DetailTemplate from 'Template/DetailTemplate';
-import ThemeList from 'components/Organism/DetailThemeList.organism';
-import BottomSection from 'components/Organism/DetailBottomSection.organims';
-import ProductDetailTop from 'components/Organism/DetailTopSection.organism';
-import Keyboard from 'components/Organism/Keyboard.organism';
-import Header from 'components/Organism/Header.organism';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import DetailTemplate from "Template/DetailTemplate";
+import ThemeList from "components/Organism/DetailThemeList.organism";
+import BottomSection from "components/Organism/DetailBottomSection.organims";
+import ProductDetailTop from "components/Organism/DetailTopSection.organism";
+import Keyboard from "components/Organism/Keyboard.organism";
+import Header from "components/Organism/Header.organism";
 
 const detailDataInterface = {
   data: {
@@ -101,7 +101,7 @@ function DetailPage() {
       }
       middleSectionComponent={<ThemeList data={data} />}
       bottomSectionComponet={<BottomSection />}
-      modalComponent={<Keyboard isModal={modalToggleFuntion} />}
+      modalComponent={<Keyboard state={isModal} isModal={modalToggleFuntion} />}
       modalValue={isModal}
     />
   );
